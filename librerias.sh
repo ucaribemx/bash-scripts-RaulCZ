8b84bc70779b71af771d1059391c1e044ea29fbf
RaulCZ <54564566+RaulCZ@users.noreply.github.com>
2019-09-24 04:56:42 -0500

    Add files via upload

---
+++ librerias.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# No es que existan las librerías pero se puede simular
+# algo similar
+# Esta es la forma de importar funciones
+source funciones.sh
+
+muestrapantalla 69 123 "epa"
+sumame 1337 3389 && echo "OK" || echo "Ocurrió un error"
+echo "Resultado: ${RESULTADO} $!"
